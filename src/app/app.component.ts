08c3e77fbc5529403b872dcf6890fc325733584e
jeffrey.sugget <jeffrey.sugget@gmail.com>
2018-04-13 17:10:52 +0800

    Refactored to have login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Riders of Western Australia';
-  message = 'Please sign in to continue';
 
-  login() {
-    this.title = 'logged in!';
-  }
-
-  signUp() {
-    this.title = 'signUp button clicked!';
-  }
-
-  forgotPassword() {
-    this.title = 'forgotPassword button clicked!';
-  }
 }
