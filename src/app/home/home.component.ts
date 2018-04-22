32192f6bbcbaed530b9a0d236fa79e6232af358a
Jeff Sugget <jeffrey.sugget@gmail.com>
2018-04-22 10:40:33 +0800

    Fixed bug where user could navigate unauthorized

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,6 +22,9 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    if (localStorage.getItem('token') === null) {
+      this.router.navigate(['/login']);
+    }
   }
 
   test() {
